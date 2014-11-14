ebe69be8f85bcc8671063bb8ddf1d50016687805
miguel Ángel ortuño <ortuman@gmail.com>
2014-11-14 10:38:31 +0100

    Updated example controller.

--- SwiftForms/ExampleFormViewController.swift
+++ SwiftForms/ExampleFormViewController.swift
@@ -60,28 +60,35 @@ class ExampleFormViewController: FormViewController, FormViewControllerDelegate 
         let section1 = FormSectionDescriptor()
         
         var row: FormRowDescriptor! = FormRowDescriptor(tag: Static.emailTag, rowType: .Email, title: "Email")
+        row.cellConfiguration = ["textField.placeholder" : "john@gmail.com", "textField.textAlignment" : NSTextAlignment.Right.rawValue]
         section1.addRow(row)
 
         row = FormRowDescriptor(tag: Static.passwordTag, rowType: .Password, title: "Password")
+        row.cellConfiguration = ["textField.placeholder" : "Enter password", "textField.textAlignment" : NSTextAlignment.Right.rawValue]
         section1.addRow(row)
         
         let section2 = FormSectionDescriptor()
         
         row = FormRowDescriptor(tag: Static.nameTag, rowType: .Name, title: "First Name")
+        row.cellConfiguration = ["textField.placeholder" : "e.g. Miguel Ángel", "textField.textAlignment" : NSTextAlignment.Right.rawValue]
         section2.addRow(row)
         
         row = FormRowDescriptor(tag: Static.lastNameTag, rowType: .Name, title: "Last Name")
+        row.cellConfiguration = ["textField.placeholder" : "e.g. Ortuño", "textField.textAlignment" : NSTextAlignment.Right.rawValue]
         section2.addRow(row)
         
         row = FormRowDescriptor(tag: Static.jobTag, rowType: .Text, title: "Job")
+        row.cellConfiguration = ["textField.placeholder" : "e.g. Entrepreneur", "textField.textAlignment" : NSTextAlignment.Right.rawValue]
         section2.addRow(row)
         
         let section3 = FormSectionDescriptor()
 
         row = FormRowDescriptor(tag: Static.URLTag, rowType: .URL, title: "URL")
+        row.cellConfiguration = ["textField.placeholder" : "e.g. gethooksapp.com", "textField.textAlignment" : NSTextAlignment.Right.rawValue]
         section3.addRow(row)
         
         row = FormRowDescriptor(tag: Static.phoneTag, rowType: .Phone, title: "Phone")
+        row.cellConfiguration = ["textField.placeholder" : "e.g. 0034666777999", "textField.textAlignment" : NSTextAlignment.Right.rawValue]
         section3.addRow(row)
         
         let section4 = FormSectionDescriptor()
@@ -136,6 +143,7 @@ class ExampleFormViewController: FormViewController, FormViewControllerDelegate 
         section5.addRow(row)
         row = FormRowDescriptor(tag: Static.categories, rowType: .MultipleSelector, title: "Categories")
         row.options = [0, 1, 2, 3, 4]
+        row.allowsMultipleSelection = true
         row.titleFormatter = { value in
             switch( value ) {
             case 0:
